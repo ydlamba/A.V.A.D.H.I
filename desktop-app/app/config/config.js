a6d44f28befe3398299e9a84759697f72133b63c
ankitkataria <ankitkataria28@gmail.com>
2018-02-05 02:16:41 +0530

    adds .gitignore

---
+++ desktop-app/app/config/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  apiEndpoint: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/",
+  apiKey: "7faeb79e89b5415a83f3513ae9662dd9",
+  serverRoot: "https://cyrex.southeastasia.cloudapp.azure.com/"
+}
+
+module.exports = CONFIG;
