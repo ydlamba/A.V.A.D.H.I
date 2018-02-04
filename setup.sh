649666447af7354847915353b0d6a678ea08ff15
Yash Dev Lamba <yashdev18@gmail.com>
2018-02-05 04:35:07 +0530

    Setup script added

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+if type "$xprintidle" > /dev/null; then
+	echo "0: Installing xprintidle ..."
+	sudo apt-get install xprintidle
+else 
+	echo "0: xprintidle Already Installed"
+fi
+
+echo "1: Giving executing permissions to script"
+chmod +x detect-inactivity.sh
+echo "2: Copying script to /usr/local/bin"
+sudo cp -p detect-inactivity.sh /usr/local/bin
+echo "3: Updating environment variable PATH"
+export PATH=$PATH:/usr/local/bin
+echo "4: Setting script to auto run at startup"
+sudo sed -i -e '1idetect-inactivity.sh & || exit 1\' /etc/rc.local 
+echo "All set successfully!"
