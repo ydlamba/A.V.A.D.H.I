5bbc78f16d01cc1d83705d2a7e773b9db8f5e5e0
Yash Dev Lamba <yashdev18@gmail.com>
2018-02-05 07:27:42 +0530

    Setup script fixed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if type "$xprintidle" > /dev/null; then
+if type $xprintidle > /dev/null; then
 	echo "0: Installing xprintidle ..."
 	sudo apt-get install xprintidle
 else 
@@ -14,5 +14,6 @@ sudo cp -p detect-inactivity.sh /usr/local/bin
 echo "3: Updating environment variable PATH"
 export PATH=$PATH:/usr/local/bin
 echo "4: Setting script to auto run at startup"
-sudo sed -i -e '1idetect-inactivity.sh & || exit 1\' /etc/rc.local 
-echo "All set successfully!"
+sudo sed -i -e '1idetect-inactivity.sh & || exit 1\n' /etc/rc.local 
+detect-inactivity.sh &
+echo "All set successfully! Script is Running in background."
