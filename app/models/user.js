e19d8a84efc9c49be2d9bbac1c919e343e9391e0
Yash Dev Lamba <yashdev18@gmail.com>
2018-02-06 05:25:26 +0530

    Model Struture Added

---
+++ app/models/user.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    name: DataTypes.STRING
+  });
+
+/* User.associate = function(models) {
+    models.User.hasMany(models.Task);
+  };*/
+
+  return User;
+};
