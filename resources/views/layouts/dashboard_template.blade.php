7ae2f050118184873a40d61dedbabd8a3b301339
ankitkataria <ankitkataria28@gmail.com>
2018-02-05 10:50:11 +0530

    moves to dashboard template and adds website head

---
+++ resources/views/layouts/dashboard_template.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Login</title>
+</head>
+<body>
+	<section class="main">
+		<header class="page-head">
+			<div class="website-name"><h3><span class="first-letter">A</span>.V.A.D.H.I</h3></div>
+			<div class="right-nav">
+				<img src="{{ asset('/images/profile.jpg')}}" class="profile-pic">
+				<form method="post" class="search-bar"><input type="text"></form>
+			</div>
+		</header>	
+		<main> 
+			<nav class="left-nav"> 
+				<ul class="main-nav">
+					<li class="nav-head">
+						MAIN 
+					</li>
+					<li class="nav-item"> 
+						<a href="#" class="nav-link">
+							<i class="fa fa-user-circle-o" aria-hidden="true"></i>
+							<div> Overview </div>
+						</a>	
+					</li>
+					<li class="nav-item"> 
+						<a href="#" class="nav-link">
+							<i class="fa fa-rss" aria-hidden="true"></i>
+							<div> Activity </div>
+						</a>
+					</li>
+				</ul>
+				<ul class="filter-nav">
+					<li class="nav-head">
+						<div>FILTER</div>
+					</li> 
+					<li class="nav-item"> 
+						<a href="#" class="nav-link">
+							<i class="fa fa-star-half-o" aria-hidden="true"></i>
+							<div> Predictions </div>
+						</a>
+					</li>
+					<li class="nav-item"> 
+						<a href="#" class="nav-link">
+							<i class="fa fa-bar-chart-o" aria-hidden="true"></i>
+							<div> Bar Graph </div>
+						</a>	
+					</li>
+					<li class="nav-item"> 
+						<a href="#" class="nav-link">
+							<i class="fa fa-pie-chart" aria-hidden="true"></i>
+							<div> Pie Chart </div>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">
+							<i class="fa fa-line-chart" aria-hidden="true"></i>
+							<div> Line Chart </div>	
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="/logout" class="nav-link">
+							<i class="fa fa-sign-out" aria-hidden="true"></i>
+							<div> Logout </div>	
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<section class="user-space">
+				<div class="user-space-head"> @yield('heading') </div>
+				<div class="user-content">
+					@yield('content')
+				</div>
+			</section>
+		</main>
+	</section>
+</body>
+</html>
