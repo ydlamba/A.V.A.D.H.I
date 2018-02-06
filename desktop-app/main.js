856effe0ee9523c72bf127b867039d37ff64c583
Deepesh Pathak <deepshpathak@gmail.com>
2018-02-07 01:51:15 +0530

    add initial setup for desktop application

---
+++ desktop-app/main.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const { app, BrowserWindow, dialog } = require("electron");
+const pug = require('electron-pug')({pretty: true});
+const path = require('path');
+const url = require('url');
+
+let mainWindow = null;
+
+function createWindow(){
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height:1400
+  })
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "app/index.pug"),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+
+  mainWindow.webContents.openDevTools()
+
+
+  mainWindow.on("closed", () => {
+    mainWindow = null
+  })
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if(process.platform !== 'darwin'){
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  if(mainWindow == null){
+    createWindow()
+  }
+});
