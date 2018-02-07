7039599b38d5db462a609b9c70fd778e727cefe2
Yash Dev Lamba <yashdev18@gmail.com>
2018-02-07 06:02:56 +0530

    HTTPS Compatible

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 require('dotenv-extended').load();
 
 var restify = require('restify');
+var fs = require('fs');
 var bot = require('./app/bot');
 var models = require('./app/models/');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+// Setup Restify Server on HTTPS
+var server = restify.createServer({
+  certificate: fs.readFileSync(process.env.PATH_CERT),
+  key: fs.readFileSync(process.env.PATH_KEY)
+});
+
+server.listen(3978, '0.0.0.0', function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
