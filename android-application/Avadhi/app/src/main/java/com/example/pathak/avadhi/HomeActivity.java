98445cfe321a365d43a6ff664706ba25f6098688
Deepesh Pathak <deepshpathak@gmail.com>
2018-03-10 23:18:59 +0530

    add home screen for avadhi mobile applictaion

---
+++ android-application/Avadhi/app/src/main/java/com/example/pathak/avadhi/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.pathak.avadhi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
